25fdb2ca8f46a4adcab45edcc10f631e27b77fd8
Kelley Lynch <kelleylynch@gmail.com>
2019-08-07 22:44:26 -0400

    adds mmif upload page for testing visualizations

--- app.py
+++ app.py
@@ -1,12 +1,47 @@
-from flask import Flask, request
+import os
+
+from flask import Flask, request, render_template, flash, redirect, url_for
 import requests
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
 @app.route('/display')
 def display_file():
-    print(request.args.get('file'))
-    return requests.get(request.args.get('file')).text
+    file = request.get_data()
+    return render_template('player_page.html', mmif=file)
+
+def upload_display(filename):
+    with open("temp/" + filename) as f:
+        file = f.read()
+    return render_template('player_page.html', mmif=file)
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file:
+            filename = secure_filename(file.filename)
+            file.save(os.path.join('temp', filename))
+            return upload_display(filename)
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form> 
+    '''
 
 @app.route('/')
 def hello_world():
@@ -14,4 +49,4 @@ def hello_world():
 
 
 if __name__ == '__main__':
-    app.run(port=4000, debug=True)
+    app.run(port=5000, host='0.0.0.0', debug=True)
