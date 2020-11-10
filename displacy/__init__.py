2fb33af9b40ffd6754bb0165d6bfcf0b95642c85
Marc Verhagen <marc@cs.brandeis.edu>
2020-11-10 16:29:28 -0500

    Fixed error with non-entities in entity view.

--- displacy/__init__.py
+++ displacy/__init__.py
@@ -26,9 +26,12 @@ def mmif_to_displacy_dict(mmif: Mmif):
     displacy_dict = {}
     ne_view = mmif.get_view_contains(Uri.NE)
     with open(transcript_location) as transcript_file:
-        displacy_dict['text'] = transcript_file.read()
-        displacy_dict['ents'] = [entity(ann) for ann in ne_view['annotations']]
         displacy_dict['title'] = None
+        displacy_dict['text'] = transcript_file.read()
+        displacy_dict['ents'] = []
+        for ann in ne_view['annotations']:
+            if ann.at_type == Uri.NE:
+                displacy_dict['ents'].append(entity(ann))
     return displacy_dict
 
 
