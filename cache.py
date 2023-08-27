7e69deef21fe3895c201c175f6e6cbe7e741d3b7
Keigh Rim <keigh.rim@gmail.com>
2023-08-27 12:11:34 -0400

    updated req.txt, increased cache size, added cache path getter helper

--- cache.py
+++ cache.py
@@ -2,48 +2,48 @@ import os
 import time
 import shutil
 import threading
+import pathlib
 
 lock = threading.Lock()
+
+
+def get_cache_path():
+    return pathlib.Path(__file__).parent.parent / "static" / "tmp"
+
 
 def set_last_access(path):
     with open(os.path.join(path, "last_access.txt"), "w") as f:
         f.write(str(time.time()))
 
-def scan_tmp_directory():
-    total_size = 0
-    oldest_accessed_dir = {"dir": None, "access_time": None}
-    for path, dirs, files in os.walk("/app/static/tmp"):
-        for f in files:
-            fp = os.path.join(path, f)
-            total_size += os.path.getsize(fp)
 
-        # Get oldest accessed directory for deletion
-        if os.path.dirname(path) != "/app/static/tmp":
-            continue
-        if not os.path.isfile(os.path.join(path, "last_access.txt")):
-            oldest_accessed_dir = {"dir": path, "access_time": 0}
+def scan_tmp_directory():
+    oldest_accessed_dir = {"dir": None, "access_time": None}
+    total_size = sum(f.stat().st_size for f in get_cache_path().glob('**/*') if f.is_file())
+    # this will be some visualization IDs
+    for p in get_cache_path().glob('*'):
+        if not (p / 'last_access.txt').exists():
+            oldest_accessed_dir = {"dir": p, "access_time": 0}
         elif oldest_accessed_dir["dir"] is None:
-            with open(os.path.join(path, "last_access.txt")) as f:
+            with open(p / 'last_access.txt') as f:
                 timestamp = f.read()
                 if timestamp == '':
                     continue
-                oldest_accessed_dir = {"dir": path, "access_time": float(timestamp)}
+                oldest_accessed_dir = {"dir": p, "access_time": float(timestamp)}
         else:
-            with open(os.path.join(path, "last_access.txt")) as f:
+            with open(p / 'last_access.txt') as f:
                 if float(f.read()) < oldest_accessed_dir["access_time"]:
                     timestamp = f.read()
                     if timestamp == '':
                         continue
-                    oldest_accessed_dir = {"dir": path, "access_time": float(timestamp)}
-    
+                    oldest_accessed_dir = {"dir": p, "access_time": float(timestamp)}
     return total_size, oldest_accessed_dir["dir"]
 
 
 def cleanup():
     with lock:
         print("Checking visualization cache...")
-        # Max tmp size is 50MB
-        max_size = 50000000
+        # Max tmp size is 500MB
+        max_size = 500000000
         folder_size, oldest_dir = scan_tmp_directory()
         while folder_size > max_size:
             print(f"Maximum cache size reached. Deleting {os.path.basename(oldest_dir)}.")
